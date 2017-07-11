fb02b3e9c1b4d92a99c1cfb84ef243d1711b31aa
zjm <zj817619@163.com>
2017-07-11 11:29:49 +0800

    增加开直播activity
    增加

---
+++ app/src/main/java/com/tarena/yy/activity/OpenActivity.java
@@ -0,0 +1,18 @@
+package com.tarena.yy.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tarena.yy.R;
+
+/**
+ * 开直播activity
+ */
+public class OpenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open);
+    }
+}
