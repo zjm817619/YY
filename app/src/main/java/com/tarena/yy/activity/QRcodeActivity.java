bd6ef1c5dc5b563ea0612d9ef9b44349fdb1a999
zjm <zj817619@163.com>
2017-07-11 18:03:08 +0800

    增加二维码activity
    增加activity

---
+++ app/src/main/java/com/tarena/yy/activity/QRcodeActivity.java
@@ -0,0 +1,15 @@
+package com.tarena.yy.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tarena.yy.R;
+
+public class QRcodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcode);
+    }
+}
